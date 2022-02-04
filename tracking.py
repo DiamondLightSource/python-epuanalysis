1ac9ae2260f04c2744146a26adda0a5590561d98
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2022-02-04 14:50:49 +0000

    Fix EPU directory structure parsing

--- tracking.py
+++ tracking.py
@@ -1,14 +1,27 @@
 import functools
-import pathlib
 import os
+import pathlib
+from typing import Dict, List, NamedTuple, Set
+
 from gemmi import cif
-from typing import Dict, List, NamedTuple
 
 
 class Micrograph(NamedTuple):
     name: pathlib.Path
     grid_square: str
     foil_hole: str
+
+
+class FoilHole(NamedTuple):
+    name: str
+    foil_hole_img: pathlib.Path
+    exposures: List[pathlib.Path]
+
+
+class GridSquare(NamedTuple):
+    name: str
+    grid_square_img: pathlib.Path
+    foil_holes: List[FoilHole]
 
 
 class EPUTracker:
@@ -27,7 +40,7 @@ class EPUTracker:
         self.column = column
         self.outdir = self.basepath / "EPU_analysis"
         if self.outdir.is_dir():
-            print(f"Directory EOU_analysis already found in {self.basepath}; removing")
+            print(f"Directory EPU_analysis already found in {self.basepath}; removing")
 
             def _remove_nonempty(dir_for_rm: pathlib.Path):
                 for element in dir_for_rm.glob("*"):
@@ -56,6 +69,7 @@ class EPUTracker:
         for p in mic.parts:
             if "GridSquare" in p:
                 return p.split("_")[-1]
+        return ""
 
     @staticmethod
     def _get_fh(mic: pathlib.Path) -> str:
@@ -63,7 +77,7 @@ class EPUTracker:
         i = split_name.index("FoilHole")
         return split_name[i + 1]
 
-    def extract(self, starfile: pathlib.Path) -> List[Micrograph]:
+    def extract(self, starfile: pathlib.Path) -> Set[Micrograph]:
         gemmi_readable = os.fspath(starfile)
         star_doc = cif.read_file(gemmi_readable)
         column_data: List[pathlib.Path] = []
@@ -76,29 +90,62 @@ class EPUTracker:
         unique_mics = {
             Micrograph(mic, self._get_gs(mic), self._get_fh(mic))
             for mic in column_data
-            if mic.endswith(self.suffix)
+            if str(mic).endswith(self.suffix)
         }
         print(f"Number of micrographs: {len(unique_mics)}")
         return unique_mics
 
     @property
     @functools.lru_cache(maxsize=1)
-    def epu_images(self) -> Dict[str, Dict[str, List[pathlib.Path]]]:
-        ims = self.epudir.glob("**/*.jpg")
-        structured_ims = {}
-        for im in ims:
-            gs = self._get_gs(im)
-            fh = self._get_fh(im)
-            if structured_ims.get(gs):
-                try:
-                    structured_ims[gs][fh].append(im)
-                except KeyError:
-                    structured_ims[gs][fh] = [im]
-            else:
-                structured_ims[gs] = {fh: [im]}
-        return structured_ims
+    def epu_images(self) -> Dict[str, GridSquare]:
+        structured_imgs = {}
+        gridsquare_dirs = [p for p in self.epudir.glob("GridSquare*")]
+        for gsd in gridsquare_dirs:
+            foilholes = [p for p in (gsd / "FoilHoles").glob("FoilHole*.jpeg")]
+            fh_data = {}
+            for fh in foilholes:
+                fh_name = "_".join(fh.name.split("_")[:2])
+                fh_data[fh_name] = FoilHole(
+                    fh_name,
+                    fh,
+                    [p for p in (gsd / "Data").glob(".jpg") if fh_name in p.name],
+                )
+            structured_imgs[gsd.name] = GridSquare(
+                gsd.name, list(gsd.glob("*.jpg"))[0], list(fh_data.values())
+            )
+        return structured_imgs
 
     def track(self):
         mics = self.extract(self.basepath / self.starfile)
         used_grid_squares = {m.grid_sqaure for m in mics}
-        
+        gui_directories = ["squares_all", "sqaures_used", "squares_not_used"]
+        all_grid_squares = set(self.epu_images.keys())
+        grid_square_names = {
+            "sqaures_all": all_grid_squares,
+            "squares_used": used_grid_squares,
+            "squares_not_used": all_grid_squares - used_grid_squares,
+        }
+        for gd in gui_directories:
+            (self.outdir / gd).mkdir()
+            for gs in grid_square_names[gd]:
+                (self.epu_images[gs].grid_square_img).symlink_to(self.outdir / gd)
+                (self.epu_images[gs].grid_square_img.with_suffix(".xml")).symlink_to(
+                    self.outdir / gd
+                )
+                (self.outdir / gd / f"{gs}_FoilHoles").mkdir()
+                (self.outdir / gd / f"{gs}_Data").mkdir()
+                for fh in self.epu_images[gs].foil_holes:
+                    (fh.foil_hole_img).symlink_to(self.outdir / gd / f"{gs}_FoilHoles")
+                    (
+                        im.symlink_to(self.outdir / gd / f"{gs}_Data")
+                        for im in fh.exposures
+                    )
+                    (fh.foil_hole_img.with_suffix(".xml")).symlink_to(
+                        self.outdir / gd / f"{gs}_FoilHoles"
+                    )
+                    (
+                        (im.with_suffix(".xml")).symlink_to(
+                            self.outdir / gd / f"{gs}_Data"
+                        )
+                        for im in fh.exposures
+                    )
